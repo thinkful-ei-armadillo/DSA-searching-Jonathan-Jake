96fa926b59688ce837183bbdfd34169b9b1be406
Jonathan Kleinpeter <joklein341@gmail.com>
2019-04-10 10:06:33 -0500

    initial commit

---
+++ search-algorithms.js
@@ -0,0 +1,24 @@
+const BinarySearchTree = require('./binary-search-tree');
+const Queue = require('./queue');
+
+function binarySearch(arr, value, start = 0, end = arr.length - 1) {
+  
+  if (start > end) {
+    return -1;
+  }
+  
+  let index = Math.floor((start + end) / 2);
+  let item = arr[index];
+
+  console.log(start, end);
+  if (item === value) {
+    return index;
+  }
+  else if (item < value) {
+    return binarySearch(arr, value, index + 1, end);
+  }
+  else if (item > value) {
+    return binarySearch(arr, value, start, index - 1);
+  }
+}
+
